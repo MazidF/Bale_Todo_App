31cbbb855c591d33e06f10b485a5175e983fbf28
m.m <m3farahani1382@gmail.com>
2022-08-24 08:48:28 +0430

    mvvm packaging

---
+++ app/src/main/java/com/example/todolist/application/App.kt
@@ -0,0 +1,7 @@
+package com.example.todolist.application
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class App : Application()
---
+++ app/src/main/java/com/example/todolist/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.todolist.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todolist.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
